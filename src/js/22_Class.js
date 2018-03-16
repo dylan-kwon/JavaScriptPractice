5f8e987877afbaee2ea0980068eb98f20606a1c1
SeokChan Kwon <chan3619@naver.com>
2018-03-16 16:30:16 +0900

    자바스크립트 공부@!@!

---
+++ src/js/22_Class.js
@@ -0,0 +1,74 @@
+/**
+ * ES6에 추가된 class keyword
+ **/
+
+
+/**
+ * 부모 클래스
+ */
+class ParentClass {
+
+    /**
+     * 생성자
+     */
+    constructor(intValue) {
+        this.intValue = intValue;
+    }
+
+
+    /**
+     * 일반 메소드
+     */
+    method() {
+        console.log("ParentClass.intValue = " + this.intValue)
+    }
+
+
+    /**
+     * static 메소드
+     */
+    static staticMethod() {
+        console.log("ParentClass.staticMethod")
+    }
+
+}
+
+
+/**
+ * 자식 클래스
+ */
+class ChildClass extends ParentClass {
+
+    /**
+     * 생성자 오버라이드
+     */
+    constructor(intValue, strValue) {
+        super(intValue);
+        this.strValue = strValue;
+    }
+
+    /**
+     * 매소드 오버라이드
+     */
+    method() {
+        super.method();
+        console.log("ParentClass.strValue = " + this.strValue)
+    }
+
+    static staticMethod() {
+        console.log("ChildClass.staticMethod()")
+    }
+
+}
+
+
+let childClassInstance = new ChildClass(1, "strValue");
+
+console.log();
+
+ParentClass.staticMethod();
+ChildClass.staticMethod();
+
+console.log();
+
+childClassInstance.method();
